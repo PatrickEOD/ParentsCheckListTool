585865e35f6d1179a892c7ebc947189db8599dda
PatrickEOD <patrykkogorek@gmail.com>
2019-04-15 06:06:38 +0200

    Starter definition of tables and some queries

--- ParentsCheckListToolAPI/src/main/resources/queries.sql
+++ ParentsCheckListToolAPI/src/main/resources/queries.sql
@@ -1,2 +1,27 @@
 create database pclt_db;
 use pclt_db;
+
+
+========================================================================================================================
+
+CREATE TABLE user (
+    id BINARY(16) PRIMARY KEY NOT NULL,
+    login VARCHAR(70) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE
+);
+
+INSERT INTO user (id, login, email) VALUES (UUID_TO_BIN(UUID()), 'jKowalski', 'j.kowalski@domain.pl');
+INSERT INTO user (id, login, email) VALUES (UUID_TO_BIN(UUID()), 'eKwiatkowska', 'e.kwiatkowska@domain.com');
+INSERT INTO user (id, login, email) VALUES (UUID_TO_BIN(UUID()), 'aKornalska', 'aKornalska@domain.com');
+========================================================================================================================
+
+CREATE TABLE user_details (
+                              id BINARY(16) PRIMARY KEY NOT NULL,
+                              login VARCHAR(70) NOT NULL UNIQUE,
+                              registration_date DATE NOT NULL,
+                              name VARCHAR(100),
+                              surname VARCHAR(120),
+                              birth_date DATE,
+                              FOREIGN KEY (login) REFERENCES user(login)
+);
+========================================================================================================================
--- assets/dbQueries/initial_Q.sql
+++ assets/dbQueries/initial_Q.sql
@@ -1,3 +1,26 @@
 create database pclt_db;
 use pclt_db;
 show tables;
+
+CREATE TABLE user (
+id BINARY(16) PRIMARY KEY NOT NULL,
+login VARCHAR(70) NOT NULL UNIQUE,
+email VARCHAR(255) NOT NULL UNIQUE
+);
+describe user;
+
+INSERT INTO user (id, login, email) VALUES (UUID_TO_BIN(UUID()), 'jKowalski', 'j.kowalski@domain.pl');
+INSERT INTO user (id, login, email) VALUES (UUID_TO_BIN(UUID()), 'eKwiatkowska', 'e.kwiatkowska@domain.com');
+INSERT INTO user (id, login, email) VALUES (UUID_TO_BIN(UUID()), 'aKornalska', 'aKornalska@domain.com');
+select BIN_TO_UUID(id) as id, login, email from user;
+
+CREATE TABLE user_details (
+id BINARY(16) PRIMARY KEY NOT NULL,
+login VARCHAR(70) NOT NULL UNIQUE,
+registration_date DATE NOT NULL,
+name VARCHAR(100),
+surname VARCHAR(120),
+birth_date DATE,
+FOREIGN KEY (login) REFERENCES user(login)
+);
+describe user_details;
